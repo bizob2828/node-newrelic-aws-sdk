91770b41dde85eb7e7dc43534caa3791074cf0cc
Bob Evans <robert.evans25@gmail.com>
2021-05-11 13:18:33 -0400

    wired up sns and sqs with v3 libs

--- nr-hooks.js
+++ nr-hooks.js
@@ -4,8 +4,20 @@
 */
 'use strict'
 
-module.exports = [{
-  type: 'conglomerate',
-  moduleName: 'aws-sdk',
-  onRequire: require('./lib/instrumentation')
-}]
+module.exports = [
+  {
+    type: 'conglomerate',
+    moduleName: 'aws-sdk',
+    onRequire: require('./lib/instrumentation')
+  },
+  {
+    type: 'message',
+    moduleName: '@aws-sdk/client-sqs',
+    onRequire: require('./lib/v3-instrumentation').bind(null, 'sqs')
+  },
+  {
+    type: 'message',
+    moduleName: '@aws-sdk/client-sns',
+    onRequire: require('./lib/v3-instrumentation').bind(null, 'sns')
+  }
+]
